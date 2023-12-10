fe7e63cadeb7044c11439e24713f16f9717d135a
rishikeshydv <rishikeshadh4@gmail.com>
2023-12-09 21:14:10 -0500

    scraper&full-stack added

--- recommender/scraper/final_scraper/single_stock.py
+++ recommender/scraper/final_scraper/single_stock.py
@@ -1,101 +1,39 @@
-#New York Stack Exchange
+# importing packages
 import requests
 import pytz
 from bs4 import BeautifulSoup
 import datetime
 import csv
 
-
+#imports for topicSpaceCreator
+from sklearn.preprocessing import LabelEncoder
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from nltk import pos_tag, word_tokenize
+import nltk
+import pandas as pd
 
 
 class individual_stock:
 
-    global price_list
-    global dm
-    global indx
     global soup
-    global i, j, k, l
 
-    r = requests.get('https://robinhood.com/collections/technology')
-    html = r.content
+    r = requests.get('https://www.allrecipes.com/gallery/top-new-recipes-2022/')
+    html = r.content 
     soup = BeautifulSoup(html,'html.parser')
 
     def __init__(self):
         self.p=None
 
-    def nam(self,j):
-        nam_list = []
+    def topicSpaceCreator(self,scrapedRecipe):
+        nltk.download('averaged_perceptron_tagger')
+        scrapedRecipeSeries = pd.Series([scrapedRecipe])
+        topicSpace = scrapedRecipeSeries.apply(lambda x: str([word for word, pos in pos_tag(word_tokenize(x)) if pos.startswith('N')]))
+        return topicSpace
 
-        for w in soup.find_all('a', {'class': 'rh-hyperlink'})[j]:
-
-            stock_name = w.text
-            nam_list = nam_list + [stock_name]
-                    
-        return nam_list
-
-         
-    def price(self, i):
-        #global i
-        price_list = []
-        
-
-        for p in soup.find_all('a', {'class': 'rh-hyperlink'})[i]:
-
-            a_price = p.text
-            a_price = a_price.replace('$', '')
-            price_list.append(a_price)
-                    
-        return price_list
-
-    def date_time(self):
-        
-        dm = []
-
-        for a in range(1):
-            dd = datetime.datetime.now(pytz.timezone("GMT"))
-            dd = dd.strftime("%Y%m%d.%H%M%S")
-            dm.append(dd)
-
-        return dm
-            
-
-            
-    def symbol(self,k):
-
-        global symb
-
-        symb = []
-
-        
-        for q in soup.find_all('a',{'class':'rh-hyperlink'})[k]:
-
-            sym = q.text
-            symb = symb + [sym]
-
-        return symb
-
-    def market_capital(self,l):
-        
-        global mc
-        mc = []
-        
-        for r in soup.find_all('a',{'class':'rh-hyperlink'})[l]:
-
-            cap = r.text
-            #cap = cap.replace('M','').replace('B','')
-            mc.append(cap)
-
-        return mc 
-
-            
-
-
-        
-
-
-
-
-
-
-
-
+    def recipe(self):
+        for w in soup.find_all('p', {'class': 'comp mntl-sc-block mntl-sc-block-html'})[:]:
+            model_input = self.topicSpaceCreator(w.text)
+                        
+        return model_input
